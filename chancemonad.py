13a1607e0c877de673cdd3ada892a2bb2861cb4c
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2013-11-30 15:06:20 +0100

    add probability and logging monad

---
+++ chancemonad.py
@@ -0,0 +1,16 @@
+
+from collections import defaultdict
+from monad import Monad
+
+class ChanceMonad(Monad):
+
+    def __init__(self, probs):
+        self.probs = probs
+
+    def bind(self, f):
+        chances = defaultdict(int)
+        for (outcome, prob) in self.probs.items():
+            for noutcome, nprob in f(outcome).items():
+                chances[noutcome] += nprob * prob
+        return ChanceMonad(chances)
+
